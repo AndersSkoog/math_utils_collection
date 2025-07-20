b0a4fc08ca5a92d5e5e6a8221a7a4a352a73d9b4
anders <anders.skooog@gmail.com>
2025-07-20 18:04:54 +0200

    added ProportionalNumber

---
+++ ProportionalNumber.py
@@ -0,0 +1,34 @@
+"""
+Instead of thinking of rational numbers a/b as simply dividing a into b parts,
+We treat it as defining a recursive field based on the integer b (the "host" integer),
+The numerator a defines how many iterations we apply of proportional recursion (or subdivision),
+This results in:
+A minimal unit (the deepest recursive subdivision),
+A maximum value (number of additions of the minimal unit).
+This reframes a/b as: a recursive steps into the field of reciprocals of integer b.
+
+the proportianal number system recognize that we should not treat rational numbers to be anything else than an expression
+of a reciprocials of an integer b where a/b = 1/a * b
+we get the minimal unit u from dividing the reciprocal 1/b with it self a number of times,
+for example if we have the rational number 7/4, then: u = ((((1/7)/7)/7)/7)/7) = (4*7) ** -4
+continuing the example where a=4 then: 1/b = (u*4)+(u*4)+(u*4)+(u*4) = 16u
+similarly the number 4 could in this example also encode the largest number in the field.
+i.e 4*(4/b) = ((16u)*4)*4 = 256u
+"""
+
+class ProportionalNumber:
+    def __init__(self, a, b):
+        self.a = a      # numerator
+        self.b = b    # denominator (base integer field)
+        self.reci = 1 / b # base reciprocal: 1/b
+        self.rational = a / b
+        self.units_in_reci = pow(b,a)
+        self.units_in_rational = self.units_in_reci * a
+        self.units_in_maxval = self.units_in_rational * pow(b,a)
+        self.unit = self.reci / self.units_in_reci
+        self.max_val = self.unit * self.units_in_maxval
+        self.field = [self.unit * i for i in range(1,self.units_in_maxval)]
+        print(self.field)
+
+
+test = ProportionalNumber(3,5)
